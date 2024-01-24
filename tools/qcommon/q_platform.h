40a8693b2ffba06da479ff644cf72c7c2ad81176
TriForceX <triforce@gznetwork.com>
2024-01-23 23:18:13 -0300

    Merge from mvdevs/mvsdk@f37abd1

--- tools/qcommon/q_platform.h
+++ tools/qcommon/q_platform.h
@@ -198,7 +198,7 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #define idx64 1
 #define ARCH_STRING "x86_64"
 #define Q3_LITTLE_ENDIAN
-#elif defined(__aarch64__) or defined(ARCH_ARM64)
+#elif defined(__aarch64__) || defined(ARCH_ARM64)
 #define ARCH_STRING "arm64"
 #define Q3_LITTLE_ENDIAN
 #endif
